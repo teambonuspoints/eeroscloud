4da358858fa0ebc778bb2b12406b5b14ceb8845a
Dan Taeyoung <provolot@gmail.com>
2016-05-07 18:06:12 -0400

    playing with distortion shader and eye gaze fly movement

---
+++ Assets/Movement/Scripts/FlyMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyMovement : MonoBehaviour {
+
+	public float thrust = 10.0f;
+	public CardboardHead head;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position += thrust * head.Gaze.direction * Time.deltaTime;
+
+		//this.transform.position += this.transform.forward * thrust;
+	}
+}
