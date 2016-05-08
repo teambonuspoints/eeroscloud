48032600e7accae3ae5122f1d8fde71d2c4582b0
Dan Taeyoung <provolot@gmail.com>
2016-05-08 17:07:31 -0400

    bounding box work and most recent master

--- Assets/Movement/Scripts/FlyMovement.cs
+++ Assets/Movement/Scripts/FlyMovement.cs
@@ -5,11 +5,13 @@ using System.Collections;
 public class FlyMovement : MonoBehaviour {
 
 	public float thrust = 10.0f;
+	public int toggleBoundingBox = 0;
 
-	private float zmin = -90.0f;
-	private float zmax = 130.0f;
-	private float xmin = -70.0f;
-	private float xmax = 130.0f;
+
+	private float xmin = -55.0f;
+	private float xmax = 125.0f;
+	private float zmin = -80.0f;
+	private float zmax = 90.0f;
 	private float ymin = -50.0f;
 	private float ymax = 50.0f;
 
@@ -17,42 +19,43 @@ public class FlyMovement : MonoBehaviour {
 	void Update () {
 		transform.position += Camera.main.transform.forward * thrust * Time.deltaTime;
 
-		Vector3 cPos = transform.position;
+		if (toggleBoundingBox != 0) {
+
+			Vector3 cPos = transform.position;
 
 
+			if (cPos.z > zmax) {
+				Vector3 newPos = Camera.main.transform.position;
+				newPos.z = zmin;
+				transform.position = newPos;
+			} 
+			if (cPos.z < zmin) {
+				Vector3 newPos = Camera.main.transform.position;
+				newPos.z = zmax;
+				transform.position = newPos;
+			}
+			if (cPos.x > xmax) {
+				Vector3 newPos = Camera.main.transform.position;
+				newPos.x = xmin;
+				transform.position = newPos;
+			}
+			if (cPos.x < xmin) {
+				Vector3 newPos = Camera.main.transform.position;
+				newPos.x = xmax;
+				transform.position = newPos;
+			}
+			if (cPos.y > ymax) {
+				Vector3 newPos = Camera.main.transform.position;
+				newPos.y = ymin;
+				transform.position = newPos;
+			}
+			if (cPos.y < ymin) {
+				Vector3 newPos = Camera.main.transform.position;
+				newPos.y = ymax;
+				transform.position = newPos;
+			}
 
-		if (cPos.z > zmax) {
-			Vector3 newPos = Camera.main.transform.position;
-			newPos.z = zmin;
-			transform.position = newPos;
-		} 
-		if (cPos.z < -1 * zmax) {
-			Vector3 newPos = Camera.main.transform.position;
-			newPos.z =  zmax;
-			transform.position = newPos;
 		}
-		if (cPos.x > xmax) {
-			Vector3 newPos = Camera.main.transform.position;
-			newPos.x = xmin;
-			transform.position = newPos;
-		}
-		if (cPos.x < -1 * xmax) {
-			Vector3 newPos = Camera.main.transform.position;
-			newPos.x = xmax;
-			transform.position = newPos;
-		}
-		if (cPos.y > ymax) {
-			Vector3 newPos = Camera.main.transform.position;
-			newPos.y =  ymin;
-			transform.position = newPos;
-		}
-		if (cPos.y < -1 * ymax) {
-			Vector3 newPos = Camera.main.transform.position;
-			newPos.y = ymax;
-			transform.position = newPos;
-		}
-
-
 
 		
 	}
